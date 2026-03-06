8827964e35fd67467cc7aa4dc4acba7989f64fd0
laljoyce0503-prog <laljoyce0503@gmail.com>
2026-03-06 11:35:32 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -187,4 +187,4 @@ def update_submission(item_id: int, payload: Dict[str, Any] = Body(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main2:app", host="0.0.0.0", port=8000, reload=True)
