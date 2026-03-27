2818dd527445dbad8a8b418ecef130a401022852
laljoyce0503-prog <laljoyce0503@gmail.com>
2026-03-27 14:35:29 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -19,10 +19,17 @@ app.add_middleware(
 )
 
 # Database Configuration
-db_config = {"host": "localhost","user": "root","password": "Joyce@0503","database": "gst_db","charset": "utf8mb4"}
+
 
 import os
-
+db_config = {
+    "host": os.getenv("MYSQLHOST"),
+    "user": os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
+    "port": int(os.getenv("MYSQLPORT")),
+    "charset": "utf8mb4"
+}
 #db_config = {"host": os.getenv("MYSQLHOST"),"user": os.getenv("MYSQLUSER"),"password": os.getenv("MYSQLPASSWORD"),"database": os.getenv("MYSQLDATABASE"),"port": int(os.getenv("MYSQLPORT"))}
 # --- Pydantic Models ---
 class Submission(BaseModel):
